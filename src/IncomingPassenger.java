77cb95bc1a94a3463b69a4fdbeb4740007399dfc
chl <1058725634@qq.com>
2021-03-07 21:39:02 +0800

    修改

--- src/IncomingPassenger.java
+++ src/IncomingPassenger.java
@@ -1,7 +1,42 @@
+import java.util.Date;
+
 public class IncomingPassenger {
     private String name;
     private String last_name;
-    private String country;
-    private String data;
+    private Country country;
+    private Date data;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
 
 }
