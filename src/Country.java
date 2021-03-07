d32c21503df36f21ee433f12475cd7c5c2afa3ac
chl <1058725634@qq.com>
2021-03-07 21:25:47 +0800

    Initial commit

---
+++ src/Country.java
@@ -0,0 +1,20 @@
+public class Country {
+    private String countryName;
+    private String quarantine_data;
+
+    public String getCountryName() {
+        return countryName;
+    }
+
+    public void setCountryName(String countryName) {
+        this.countryName = countryName;
+    }
+
+    public String getQuarantine_data() {
+        return quarantine_data;
+    }
+
+    public void setQuarantine_data(String quarantine_data) {
+        this.quarantine_data = quarantine_data;
+    }
+}
---
+++ src/IncomingPassenger.java
@@ -0,0 +1,7 @@
+public class IncomingPassenger {
+    private String name;
+    private String last_name;
+    private String country;
+    private String data;
+
+}
